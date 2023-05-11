415a6bee770d09c6f704646f856c5ad11be9a569
Shashwot KC <shashwot_07@hotmail.com>
2023-05-11 15:35:27 +0700

    Add telescope config

--- init.lua
+++ init.lua
@@ -36,7 +36,7 @@ plug('nvim-treesitter/nvim-treesitter')
 plug('nvim-telescope/telescope.nvim')
 plug('nvim-lua/plenary.nvim')
 plug('nvim-lua/popup.nvim')
-
+plug('rose-pine/neovim')
 
 vim.cmd('call plug#end()')
 
@@ -50,8 +50,63 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+require('rose-pine').setup({
+	--- @usage 'auto'|'main'|'moon'|'dawn'
+    variant = 'moon',
+	--- @usage 'main'|'moon'|'dawn'
+	dark_variant = 'moon',
+	bold_vert_split = false,
+	dim_nc_background = false,
+	disable_background = false,
+	disable_float_background = false,
+	disable_italics = true,
 
+	--- @usage string hex value or named color from rosepinetheme.com/palette
+	groups = {
+		background = 'base',
+		background_nc = '_experimental_nc',
+		panel = 'surface',
+		panel_nc = 'base',
+		border = 'highlight_med',
+		comment = 'muted',
+		link = 'iris',
+		punctuation = 'subtle',
+
+		error = 'love',
+		hint = 'iris',
+		info = 'foam',
+		warn = 'gold',
+
+		headings = {
+			h1 = 'iris',
+			h2 = 'foam',
+			h3 = 'rose',
+			h4 = 'gold',
+			h5 = 'pine',
+			h6 = 'foam',
+		}
+		-- or set all headings at once
+		-- headings = 'subtle'
+	},
+
+	-- Change specific vim highlight groups
+	-- https://github.com/rose-pine/neovim/wiki/Recipes
+	highlight_groups = {
+		ColorColumn = { bg = 'rose' },
+
+		-- Blend colours against the "base" background
+		CursorLine = { bg = 'foam', blend = 10 },
+		StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+	}
+})
+
+-- Set colorscheme after options
+vim.cmd('colorscheme rose-pine')
+local transform_mod = require('telescope.actions.mt').transform_mod
+
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 require'nvim-tree'.setup {
   disable_netrw       = true,
   hijack_netrw        = true,
